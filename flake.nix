e243d98efa0ecfdc222b7fad6c9e63e30868c5d4
atalie2m <atalie@atalie.net>
2025-07-23 22:51:56 +0900

    refactor(nix): restructure brew-nix configuration for improved clarity and organization

--- flake.nix
+++ flake.nix
@@ -7,14 +7,16 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    brew-nix.url = "github:BatteredBunny/brew-nix";
-    brew-nix.inputs.nixpkgs.follows = "nixpkgs";
-    brew-nix.inputs.nix-darwin.follows = "nix-darwin";
     brew-api = {
       url = "github:BatteredBunny/brew-api";
       flake = false;
     };
-    brew-nix.inputs.brew-api.follows = "brew-api";
+    brew-nix = {
+      url = "github:BatteredBunny/brew-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-darwin.follows = "nix-darwin";
+      inputs.brew-api.follows = "brew-api";
+    };
   };
 
   outputs = inputs: import ./outputs.nix inputs;
