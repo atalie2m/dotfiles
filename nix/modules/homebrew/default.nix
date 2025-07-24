50bd408c2b22738c20bb82199480fa62f4354bff
atalie2m <atalie@atalie.net>
2025-07-25 02:49:52 +0900

    refactor(nix): clean up comments and correct application name in brew-nix configuration

--- nix/modules/homebrew/default.nix
+++ nix/modules/homebrew/default.nix
@@ -1,17 +1,14 @@
 { config, pkgs, brew-nix, ... }:
 
 {
-  # 1. overlay を明示的に追加
   nixpkgs.overlays = [ brew-nix.overlays.default ];
 
-  # 2. Enable brew-nix
   brew-nix = {
     enable = true;
   };
 
-  # 3. Install applications using brew-nix
   environment.systemPackages = with pkgs; [
-    brewCasks.rio    # Rio terminal
-    brewCasks.keycue # KeyCue (corrected from keyclu)
+    brewCasks.rio
+    brewCasks.keyclu
   ];
 }
