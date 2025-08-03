4d0438cb97f781831796891b51fefa4c73e32b87
atalie2m <atalie@atalie.net>
2025-08-03 15:56:41 +0900

    feat(nix): replace plist manipulation with AppleScript for Terminal font configuration

--- nix/modules/home/programs/terminal.nix
+++ nix/modules/home/programs/terminal.nix
@@ -1,41 +1,44 @@
 { pkgs, lib, ... }:
 
 let
-  # NSfont object for 0xProto Nerd Font
-  fontData = builtins.readFile ./font.data;
+  terminalConfigScript = pkgs.writeScript "configure-terminal" ''
+    #!/usr/bin/osascript
+
+    set targetFont to "0xProto Nerd Font"
+    set targetSize to 11
+    set profileName to "Atalie's dotfiles - Standard"
+
+    tell application "Terminal"
+      activate
+
+
+      -- Create or get profile
+      try
+        set protoSettings to settings set profileName
+      on error
+        set newSettings to (default settings)
+        set name of newSettings to profileName
+        set protoSettings to newSettings
+      end try
+
+      -- Apply font settings
+      try
+        set font name of protoSettings to targetFont
+        set font size of protoSettings to targetSize
+        set default settings to protoSettings
+        set startup settings to protoSettings
+        display notification "`Atalie's dotfiles - Standard` configured: " & targetFont & " " & targetSize & "pt"
+      on error
+        display dialog "Failed to set font '" & targetFont & "'. Font may not be installed." buttons {"OK"} default button "OK" with icon caution
+      end try
+    end tell
+  '';
 in
 {
-  # Terminal.app font settings - using extracted NSFont blob for 0xProto Nerd Font
-  # Font blob extracted from manually configured Terminal.app Basic profile
+  # Terminal.app font configuration using AppleScript
+  # More maintainable than direct plist manipulation
 
-  home.file."Library/Preferences/com.apple.Terminal.plist".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Default Window Settings</key>
-      <string>0xProto</string>
-      <key>Startup Window Settings</key>
-      <string>0xProto</string>
-      <key>Window Settings</key>
-      <dict>
-        <key>0xProto</key>
-        <dict>
-          <key>Font</key>
-          <data>${fontData}</data>
-          <key>FontAntialias</key>
-          <true/>
-          <key>FontWidthSpacing</key>
-          <real>1.004032258064516</real>
-          <key>name</key>
-          <string>0xProto</string>
-          <key>ProfileCurrentVersion</key>
-          <real>2.07</real>
-          <key>type</key>
-          <string>Window Settings</string>
-        </dict>
-      </dict>
-    </dict>
-    </plist>
+  home.activation.configureTerminal = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD ${terminalConfigScript}
   '';
 }
