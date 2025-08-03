b0b394c015e04c37ae68b61441e087ca0fb9d37e
atalie2m <atalie@atalie.net>
2025-08-03 15:23:07 +0900

    feat(nix): fonts configuration in Darwin module

--- nix/modules/home/fonts.nix
+++ nix/modules/home/fonts.nix
@@ -1,15 +1,5 @@
-{ pkgs, ... }:
+_:
 
 {
-  home.packages = with pkgs; [
-    # Nerd Fonts (for terminal and code editors)
-    nerd-fonts.jetbrains-mono
-    nerd-fonts._0xproto
-
-    # Google Fonts
-    roboto
-    roboto-mono
-  ];
-
   fonts.fontconfig.enable = true;
 }
