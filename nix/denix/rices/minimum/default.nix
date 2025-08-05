8bfc85fd0d0499855137c9b6ecad5c35f590b537
atalie2m <atalie@atalie.net>
2025-08-06 00:24:41 +0900

    modulise: replace program enable flags with imports for GPG and Git configurations

--- nix/denix/rices/minimum/default.nix
+++ nix/denix/rices/minimum/default.nix
@@ -4,7 +4,9 @@ delib.rice {
   name = "minimum";
 
   home = { name, cfg, myconfig, ... }: {
-    programs.gpg.enable = true;
-    programs.git.enable = true;
+    imports = [ 
+      ../../../modules/home/programs/gpg.nix
+      ../../../modules/home/programs/git.nix
+    ];
   };
 }
