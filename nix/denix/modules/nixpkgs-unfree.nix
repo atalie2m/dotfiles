c39d9cf73670b895ded5161231552e20221673f7
atalie2m <atalie@atalie.net>
2025-08-06 12:54:50 +0900

    feat: update nixpkgs-unfree module to include lib dependency and improve package handling

--- nix/denix/modules/nixpkgs-unfree.nix
+++ nix/denix/modules/nixpkgs-unfree.nix
@@ -1,4 +1,4 @@
-{ delib, ... }:
+{ delib, lib, ... }:
 
 # Allow select unfree packages on both Home Manager and Darwin
 
@@ -7,8 +7,14 @@ delib.module {
 
   options.nixpkgs.unfree = with delib.options; {
     enable = boolOption false;
+    packages = listOfOption str [ "claude-code" ];
   };
 
-  home.always.imports = [ ../../modules/nixpkgs/unfree.nix ];
-  darwin.always.imports = [ ../../modules/nixpkgs/unfree.nix ];
+  home.ifEnabled = { cfg, ... }: {
+    nixpkgs.config.allowUnfreePredicate = pkg: lib.elem (lib.getName pkg) cfg.packages;
+  };
+
+  darwin.ifEnabled = { cfg, ... }: {
+    nixpkgs.config.allowUnfreePredicate = pkg: lib.elem (lib.getName pkg) cfg.packages;
+  };
 }
