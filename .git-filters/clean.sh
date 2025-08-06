502a9f801f7fa8a0757f3f30d6594bc17580421f
atalie2m <atalie@atalie.net>
2025-08-07 01:21:06 +0900

    Fix: Re-add missing smudge.sh file

--- .git-filters/clean.sh
+++ .git-filters/clean.sh
@@ -6,8 +6,16 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
 # Replace all system information with their respective placeholders
+<<<<<<< HEAD
 perl -0 -pe "s|\"\Q$COMPUTER_NAME\E\"|\"u1’s MacBook Air\"|g; \
           s|\"\Q$SERIALIZED_COMPUTER_NAME\E\"|\"u1’s-MacBook-Air\"|g; \
           s|\"\Q$LOCAL_HOSTNAME\E\"|\"u1s-MacBookAir\"|g; \
           s|\"\Q$USER_NAME\E\"|\"u1\"|g; \
           s|\Q/Users/$USER_NAME\E|/Users/u1|g"
+=======
+perl -0 -pe "s|\"\Q$COMPUTER_NAME\E\"|\"u1’s MacBook Air\"|g; \
+          s|\"\Q$SERIALIZED_COMPUTER_NAME\E\"|\"u1’s-MacBook-Air\"|g; \
+          s|\"\Q$LOCAL_HOSTNAME\E\"|\"u1s-MacBookAir\"|g; \
+          s|\"\Q$USER_NAME\E\"|\"u1\"|g; \
+          s|\Q/Users/$USER_NAME\E|/Users/u1|g"
+>>>>>>> parent of b86a51d (feat: implement CI setup script for Git filters and streamline filter application process)
