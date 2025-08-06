962d066badefa90d96a017009a3c77b69380c84d
atalie2m <atalie@atalie.net>
2025-08-06 12:50:38 +0900

    feat: enhance GPG module with agent configuration and session variable support

--- nix/denix/modules/gpg.nix
+++ nix/denix/modules/gpg.nix
@@ -1,11 +1,41 @@
-{ delib, ... }:
+{ delib, pkgs, ... }:
 
+# GPG and GPG agent configuration
 delib.module {
   name = "gpg";
 
   options.gpg = with delib.options; {
     enable = boolOption false;
+
+    agent = {
+      enableSshSupport = boolOption true;
+      enableExtraSocket = boolOption true;
+      defaultCacheTtl = intOption 1800;
+      defaultCacheTtlSsh = intOption 1800;
+      maxCacheTtl = intOption 7200;
+    };
   };
 
-  home.always.imports = [ ../../modules/home/programs/gpg.nix ];
+  home.ifEnabled = { cfg, ... }: {
+    programs.gpg.enable = true;
+
+    services.gpg-agent = {
+      enable = true;
+      enableSshSupport = cfg.agent.enableSshSupport;
+      enableExtraSocket = cfg.agent.enableExtraSocket;
+      pinentry.package = pkgs.pinentry_mac;
+
+      defaultCacheTtl = cfg.agent.defaultCacheTtl;
+      defaultCacheTtlSsh = cfg.agent.defaultCacheTtlSsh;
+      maxCacheTtl = cfg.agent.maxCacheTtl;
+
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+
+    home.sessionVariables = {
+      SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
+      GPG_TTY = "$(tty)";
+    };
+  };
 }
